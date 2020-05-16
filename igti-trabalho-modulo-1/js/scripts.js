2d8ab674561285767a0595c0a248ff42a982c601
Felipe Bogo <felipe.bogo@jupiter.co.ao>
2020-05-17 00:28:54 +0100

    inclusao

---
+++ igti-trabalho-modulo-1/js/scripts.js
@@ -0,0 +1,31 @@
+
+function init() {
+  var textR = document.querySelector('#text-r');
+  var textG = document.querySelector('#text-g');
+  var textB = document.querySelector('#text-b');
+  var boxResult = document.querySelector('#box-result');
+
+
+  function setText(range) {
+    var text = range.parentElement.querySelector('input[type="text"]');
+    text.value = range.value;
+  }
+
+  function rangeHandler(event) {
+    setText(event.target);
+    drawBoxResult();
+  }
+
+  function drawBoxResult() {
+    boxResult.style.backgroundColor = `rgb(${textR.value},${textG.value},${textB.value})`;
+  }
+
+  var ranges = document.querySelectorAll('input[type=range]');
+  ranges.forEach(range => {
+    range.addEventListener('change', rangeHandler);
+    setText(range);
+  });
+  drawBoxResult();
+}
+window.addEventListener('load', init);
+
