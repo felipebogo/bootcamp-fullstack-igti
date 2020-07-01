d034605b8d966cf720422e961b8b08ba1f0d610f
Felipe Bogo <felipe.bogo@jupiter.co.ao>
2020-07-01 08:48:24 +0100

    removido trabalho do repositorio modulo 4

--- Modulo-4-banco/trabalho-pratico/src/routes/index.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import accountsRoute from './accounts.routes.js';
-
-const routes = express.Router();
-
-routes.use('/accounts',accountsRoute);
-
-
-export default routes;
